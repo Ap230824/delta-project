a4ed3fae2ab95e993384ddf06efc1fbfc53a7835
Aakash Podar <164460415+Ap230824@users.noreply.github.com>
2024-05-04 08:32:35 +0530

    file added

--- public/Js/map.js
+++ public/Js/map.js
@@ -1,1 +1,21 @@
 
+
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // choose from mapbox's core styles , or make yourmown style withnmapbox studio
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: listing.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+
+// console.log(coordinates);
+
+// to create by default map marker
+const marker = new mapboxgl.Marker({color : "red"})
+ .setLngLat(listing.geometry.coordinates)  //listing.geometry.coordinates
+ .setPopup(new mapboxgl.Popup({offset:25})
+ .setHTML(` <h4>${listing.title}</h4><p>exact location will be provided after booking</p>`))
+ .addTo(map);
